7505a3ed6ca4d6af626e44283a7243e07740405c
Rodrigo Santos <rjrodrigosantos12@gmail.com>
2024-05-04 16:52:52 -0300

    update create cards food and drinks

--- src/ui/index.js
+++ src/ui/index.js
@@ -10,49 +10,96 @@ function app(){
         e.preventDefault;
         goInsta()})
 
-    dados.map( (item)=>{
-        createCard(item.urlIMG, item.title, item.description, item.priceReal, item.priceCent)
-    })
+    let sectionFoods = document.querySelector('.foods');
+    let sectionBebidas = document.querySelector('.bebidas');
+    let sectionDoces = document.querySelector('.doces');
+
+    dados.foods.map( (item)=>{
+        createCard(item.urlIMG, item.title, item.description, item.priceReal, item.priceCent, sectionFoods)
+    });
+    dados.bebidas.map( (item)=>{
+        createCard(item.urlIMG, item.title, item.description, item.priceReal, item.priceCent, sectionBebidas)
+    });
+    dados.doces.map( (item)=>{
+        createCard(item.urlIMG, item.title, item.description, item.priceReal, item.priceCent, sectionDoces)
+    });
     
     
 }    
 
 document.addEventListener("DOMContentLoaded", app());
 
+document.addEventListener('click',(e)=>{
+    let elemento = e.srcElement.classList.value
+    if(elemento == 'image-products'){
+        createIMGSample(e.srcElement.src)
+    }
+    if (e.srcElement.id == 'closeIMGSample'){
+        const elementIMGSample = document.querySelector('.containerIMGSample')
+        elementIMGSample.remove();
+        console.log('containerIMGSample')
+    }
+});
 
-function createCard(urlIMG,title,description,priceReal,priceCent){
-    let Elemento = `<article>
-    <img src="${urlIMG}" alt="" class="image-products">
+function createCard(urlIMG,title,description,priceReal,priceCent,sectionFood){
 
-    <div class="wrapper">
-
-        <div class="information">
-
-            <h3 class="title-third">${title}</h3>
-            <p class="description">${description}</p>
-
-        </div>
-        <div class="information-secund">
-
-            <img src="../public/like.svg" alt="" class="like">
-            <div class="price">R$<span>${priceReal}</span>${priceCent}</div>
-
-        </div>
-    </div>
+    let article = document.createElement('article');
+    let image = document.createElement('img');
+    image.src = `${urlIMG}`;
+    image.classList.add('image-products');
     
-</article>`
+    let wrapper = `
+        <div class="wrapper">
 
-let sectionDoces = document.querySelector('.doces')
+            <div class="information">
 
-sectionDoces.innerHTML += Elemento;
+                <h3 class="title-third">${title}</h3>
+                <p class="description">${description}</p>
 
+            </div>
+            <div class="information-secund">
+
+                <img src="../public/like.svg" alt="" class="like">
+                <div class="price">R$<span>${priceReal}</span>${priceCent}</div>
+
+            </div>
+        </div>
+    `
+
+
+    // image.addEventListener('click',)
+
+    article.appendChild(image)
+    article.innerHTML += wrapper;
+
+
+
+
+
+
+sectionFood.appendChild(article);
+
+
+}
+
+function createIMGSample(urlIMG){
+    
+    let elementIMGSample = `
+    <div class="containerIMGSample">
+                <div id="closeIMGSample">X</div>
+                <img  class="IMGSample" style="background-image: url('${urlIMG}');">
+    </div>`
+
+    let sectionDoces = document.querySelector('.doces')
+
+    sectionDoces.innerHTML += elementIMGSample;
 
 }
 
 async function fetchData (){
     const response  = await fetch('./data/doces.json')
     const data  = await response.json();
-    return data.doces
+    return data
 }
 
 function goInsta(){
